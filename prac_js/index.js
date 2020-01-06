85a8add85d72f05ccb7585dc6092d2dbd0d5efd9
akhilsharma12297 <akhil.sharma12297@gmail.com>
2020-01-06 14:39:44 +0530

    Code

---
+++ prac_js/index.js
@@ -0,0 +1,14 @@
+
+console.log("Akhil Sharma");
+
+const temp = "akhil Sharma";
+
+console.log("the name us &{name}");
+
+console.log(`the name us ${ temp }.`);
+
+
+while (true) {
+    console.log(3.141592);
+}
+
