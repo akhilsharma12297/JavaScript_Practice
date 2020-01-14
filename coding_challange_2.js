0375e26d9dac0dca8f22929bff472186722cba69
akhilsharma12297 <akhil.sharma12297@gmail.com>
2020-01-14 17:13:07 +0530

    Code

---
+++ coding_challange_2.js
@@ -0,0 +1,30 @@
+
+class BinarySearchTree 
+{ 
+    constructor() 
+    { 
+        // root of a binary seach tree 
+        this.root = null; 
+    } 
+function Node {
+  constructor(val) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+
+this.root = make_BST(0, 9, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+
+make_BST(lo, hi, arr);
+{
+  if (lo > hi) {
+    return null;
+  }
+
+  mid = (lo + hi) / 2;
+
+  Node node = new Node(); 
+}
+}
